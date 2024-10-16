748a107d1d91f7839b4cb8eb4d092d5cd09e49cd
SerranilloRoman <72419082+SerranilloRoman@users.noreply.github.com>
2024-10-15 21:58:15 -0600

    Add files via upload

---
+++ Hands-on 3/helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello World"
+echo
+echo "Listando el contenido del directorio actual:"
+ls
+echo
+echo "Creando directorio 'Test'..."
+mkdir Test
+echo "Cambiandonos al directorio 'Test'..."
+cd Test
+echo "Listando el contenido del directorio 'Test' (deberia estar vacio XD):"
+ls
